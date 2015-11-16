fed50fbb5a2b5ea80eb944515e21ceba12fb8d82
Ar-rT <ar-rt@bk.ru>
2015-11-16 15:48:54 +0300

    First upload

---
+++ RightTriangle/RightTriangleMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangle
+{
+    public static class RightTriangleMath
+    {
+        /// <summary>
+        /// Calculates area of right trangle square
+        /// </summary>
+        /// <param name="side1Length">length of first side</param>
+        /// <param name="side2Length">length of second side</param>
+        /// <param name="side3Length">length of third side</param>
+        public static Double Area(Double side1Length, Double side2Length, Double side3Length)
+        {
+            var sidesLength = new[] { side1Length, side2Length, side3Length };
+            var hypotenuse = sidesLength.Max();
+            try
+            {
+                sidesLength.Single(l => DoubleEquals(l, hypotenuse));//checks if there is only one side with maximum length
+            }
+            catch (System.InvalidOperationException)
+            {
+                throw new ArgumentException("Wrong sides length");
+            }
+            if (!DoubleEquals(hypotenuse, Math.Sqrt(sidesLength.Where(l => l != hypotenuse).Select(l => Math.Pow(l, 2)).Sum())))//checks Pythagorean theorem
+                throw new ArgumentException("Wrong sides length, does not satisfy the Pythagorean theorem");
+            return 0.5 * sidesLength.Where(l => !DoubleEquals(l, hypotenuse)).Aggregate((cathetus1, cathetus2) => cathetus1 * cathetus2);
+        }
+
+        private static bool DoubleEquals(double first, double second)
+        {
+            return Math.Abs(first - second) <= Double.Epsilon;
+        }
+    }
+}
---
+++ UnitTests/RightTriangleUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RightTriangle;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RightTriangleUnitTests
+    {
+        [TestMethod]
+        public void RightTriangleAreaTests()
+        {
+            Double Side1 = 4;
+            Double Side2 = 5;
+            Double Side3 = Math.Sqrt(Math.Pow(Side1,2) + Math.Pow(Side2,2));
+            RightTriangleMath.Area(Side1, Side2, Side3);//Right parameters
+            Side1 = 10;
+            Side2 = 10;
+            Side3 = 10;
+            try
+            {
+                RightTriangleMath.Area(Side1, Side2, Side3);
+            }
+            catch (ArgumentException)//All sides are the same length
+                {
+                }
+            Side1 = 7;
+            Side2 = 8;
+            Side3 = 9;
+            try
+            {
+                RightTriangleMath.Area(Side1, Side2, Side3);
+            }
+            catch (ArgumentException)//Does not satisfy the Pythagorean theorem
+            {
+            }
+        }
+    }
+}
